ffe83bc88c5a4184d4973b1197e01018e635a0b5
Andriy <91940935+StopFuture@users.noreply.github.com>
2023-11-05 17:58:31 +0200

    Added helpers(eps-producers, files reading)

--- grammar/grammar.py
+++ grammar/grammar.py
@@ -3,11 +3,17 @@ from grammar.terminal import Terminal
 import re
 import itertools
 
+
 class Grammar:
     def __init__(self, rules):
         self.terminals = []
         self.non_terminals = []
         self.get_rules(rules)
+        self.rules = rules
+        self.epsilon_producers = self.find_epsilon_producing_non_terminals()
+
+    def get_epsilon(self):
+        return next((t for t in self.terminals if t.is_empty()))
 
     @staticmethod
     def get_regex(text_set):
@@ -41,3 +47,52 @@ class Grammar:
                         n.rules[-1].append(next((nt for nt in self.non_terminals if nt.text == m.group(1))))
                     elif m.group(2):
                         n.rules[-1].append(next((t for t in self.terminals if t.text == m.group(2))))
+
+    def find_epsilon_producing_non_terminals(self):
+        epsilon_producers = set()
+        # Finding non-terminals that have a direct epsilon production
+        for nt in self.non_terminals:
+            for production in nt.rules:
+                if any(isinstance(symbol, Terminal) and symbol.is_empty() for symbol in production):
+                    epsilon_producers.add(nt)
+                    break
+
+        # Recursive: finding non-terminals that can produce epsilon through other non-terminals
+        added = True
+        while added:
+            added = False
+            for nt in self.non_terminals:
+                if nt in epsilon_producers:
+                    continue
+                for production in nt.rules:
+                    # If all elements in the production are epsilon producers or empty terminals, this one is too
+                    if all(symbol in epsilon_producers or (isinstance(symbol, Terminal) and symbol.is_empty()) for
+                           symbol in production):
+                        epsilon_producers.add(nt)
+                        added = True
+                        break
+
+        return epsilon_producers
+
+    @staticmethod
+    def read_grammar_from_file(file_path):
+        rules = {}
+
+        with open(file_path, 'r') as file:
+            for line in file:
+                parts = line.strip().split('->')
+                if len(parts) == 2:
+                    left, right = parts
+                    left = left.strip()
+
+                    right_productions = right.strip().split("|")
+                    if left not in rules:
+                        rules[left] = right_productions
+                    else:
+                        rules[left].extend(right_productions)
+
+        # Replace any occurrence of 'epsilon' with 'ε'
+        for key in rules.keys():
+            rules[key] = ['ε' if prod == 'epsilon' else prod for prod in rules[key]]
+
+        return rules
--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 from grammar.grammar import Grammar
+from grammar.firstfollow import FirstFollow
+from grammar.non_terminal import NonTerminal
+from grammar.terminal import Terminal
 
 if __name__ == "__main__":
     test_grammar = Grammar({
-        'S': ['BA'], 
+        'S': ['BA'],
         'A': ['+BA', 'ε'],
         'B': ['DC'],
         'C': ['*DC', 'ε'],
         'D': ['(S)', 'a']
     })
 
+    test_grammar_1 = Grammar({
+        'E': ['id + D', '(E * R)', 'epsilon'],
+        'D': ['V * E', 'L ! E'],
+        'R': ['V ! E', 'L * E'],
+        'V': ['Z', 'num'],
+        'L': ['Z', '(E)'],
+        'Z': ['epsilon'],
+    })
+
+    test_grammar_conflict = Grammar({
+        'S': ['E', 'E a'],
+        'E': ['b', 'epsilon']
+    })
+
     # print(test_grammar.non_terminals)
+    first_follow = FirstFollow(test_grammar)
+    first_k = first_follow.first_k(1)
+    # print(first_follow.tuples_to_strings(first_follow.first_k(2)))
+    print(first_follow.tuples_to_strings(first_follow.follow_k(1, first_k)))
+    # print(f)
+    # print(first_follow.concat_k(1, {(Terminal('ε'),)}, {(Terminal('ε'),), (Terminal('+'),)}))
+
+
+    test_grammar_2 = Grammar({
+        'E': ['id + D', '( E * R )', 'ε'],
+        'D': ['V * E', 'L ! E'],
+        'R': ['V ! E'],
+        'V': ['Z', 'num'],
+        'L': ['Z', '( E )'],
+        'Z': ['ε']
+    })
+
+    test_grammar_3 = Grammar(Grammar.read_grammar_from_file("inputs/input_1.txt"))
+    # print(test_grammar.non_terminals)
+
+    print(test_grammar.epsilon_producers)
+    print(test_grammar_2.epsilon_producers)
+    print(test_grammar_3.epsilon_producers)
