8b2c7b4ec926bd33c773eff1ee9cc9bcab1fed62
BogdanTurbal <bogdan.turbal.y@gmail.com>
2023-11-05 21:23:24 +0200

    Cleared main

--- main.py
+++ main.py
@@ -4,52 +4,6 @@ from grammar.non_terminal import NonTerminal
 from grammar.terminal import Terminal
 from grammar.parser import ParsingTableBuilder
 
-
-def build_parsing_table(grammar, first_sets, follow_sets):
-    """Build the LL(1) parsing table for a given grammar."""
-    parsing_table = {}
-    
-    for i, (non_terminal, productions) in enumerate(grammar.rules.items()):
-        for production in productions:
-            # For each terminal in FIRST(production)
-            first_production = set()
-            #print(non_terminal)
-            #print(production)
-            for t_symbol in production:
-                symbol = Terminal(t_symbol) if Terminal(t_symbol) in grammar.terminals else NonTerminal(t_symbol)
-                first_symbol = first_sets[symbol] if symbol in first_sets else {(symbol, )}
-                #print(first_symbol)
-                if Terminal('ε') not in first_symbol:
-                    #print(first_symbol)
-                    first_production.update(first_symbol)
-                if 'ε' not in first_symbol:
-                    break
-            else:
-                first_production.add(Terminal('ε'))
-            #print()
-            #print(first_production)
-            for terminall in first_production:
-                #print(terminal)
-                terminal = terminall[0]
-                #print(terminal[0])
-                #print(Terminal('ε'))
-                if terminal != Terminal('ε'):
-                    if (non_terminal, terminal) not in parsing_table:
-                        parsing_table[(non_terminal, terminal)] = production
-                    else:
-                        raise ValueError(f"Grammar is not LL(1): Conflict at ({non_terminal}, {terminal})")
-
-            # If ε is in FIRST(production), for each terminal in FOLLOW(non_terminal)
-            if (Terminal('ε'), ) in first_production:
-                #print('okdfdkflkdlfk')
-                for terminal in follow_sets[NonTerminal(non_terminal)]:
-                    if (non_terminal, terminal) not in parsing_table:
-                        parsing_table[(non_terminal, terminal)] = (production, i)
-                    else:
-                        raise ValueError(f"Grammar is not LL(1): Conflict at ({non_terminal}, {terminal})")
-
-    return parsing_table
-
 if __name__ == "__main__":
     test_grammar = Grammar({
         'S': ['BA'],
