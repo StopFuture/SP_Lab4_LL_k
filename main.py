0f0952e4f845d9a2c6d81e48db3ba4d41e8c3388
Andriy Fedorych <91940935+StopFuture@users.noreply.github.com>
2023-11-06 02:38:06 +0200

    demonstration of work

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from grammar.grammar import Grammar
 from grammar.firstfollow import FirstFollow
-from grammar.non_terminal import NonTerminal
-from grammar.terminal import Terminal
+from grammar.ast import ASTNode
 from grammar.parser import ParsingTableBuilder, LL1Parser, parse_grammar
+from grammar.recursive_descent import RecursiveDescentParser
 
 
+def parse_and_display(grammar, str_to_parse):
+    first_follow = FirstFollow(grammar)
+    first_k = first_follow.first_k(1)
+    follow_k = first_follow.follow_k(1, first_k)
+    print("Terminals: ", grammar.terminals)
+    print("Non-Terminals: ", grammar.non_terminals)
+    print("Epsilon-Producers: ", grammar.epsilon_producers)
+    print("First_k", first_k)
+    print("Follow_k", follow_k)
 
-# Example usage:
-# parser = LL1Parser(parsing_table, start_symbol)
-# parser.parse(list_of_tokens)
+    recursive_parser = RecursiveDescentParser(grammar)
+    print(f'Recursive Descent Parsing "{str_to_parse}": {recursive_parser.parse(str_to_parse)}')
+    sequence = parse_grammar(grammar, str_to_parse)
+    print('-' * 100)
+    print("Applied Rules:")
+    for i, rule in enumerate(sequence):
+        print(f"Rule № {i + 1}: {rule}")
+
+    print('-' * 100)
+    print("Abstract Syntax Tree:")
+    root = test_grammar.build_ast(sequence)
+    ASTNode.print_ast(root)
+    print('-' * 100)
 
 
 if __name__ == "__main__":
+    print("TEST 1:")
     test_grammar = Grammar({
         'S': ['BA'],
         'A': ['+BA', 'ε'],
@@ -19,58 +39,16 @@ if __name__ == "__main__":
         'C': ['*DC', 'ε'],
         'D': ['(S)', 'a']
     })
-
-    test_grammar_1 = Grammar({
-        'E': ['id + D', '(E * R)', 'epsilon'],
-        'D': ['V * E', 'L ! E'],
-        'R': ['V ! E', 'L * E'],
-        'V': ['Z', 'num'],
-        'L': ['Z', '(E)'],
-        'Z': ['epsilon'],
-    })
-
-
-    test_grammar_conflict = Grammar({
-        'S': ['E', 'E a'],
-        'E': ['b', 'epsilon']
-    })
-
-
-    # first_follow = FirstFollow(test_grammar)
-    # first_k = first_follow.first_k(1)
-    # follow_k = first_follow.follow_k(1, first_k)
-    # parser = ParsingTableBuilder(test_grammar, first_k, follow_k)
-    # table = parser.build()
-    # print("Parsing table")
-    # print(table)
-    # parser = LL1Parser(table, NonTerminal('S'), test_grammar)
     str_to_parse = '(a+a)*a'
-    
-    applied_rules = parse_grammar(test_grammar, str_to_parse)
-    print(applied_rules)
-    #applied_rules list of (A, terminal, w) where A -> w
-    print('-' * 100)
-    # print(first_follow.tuples_to_strings(first_follow.first_k(2)))
-    #print(first_follow.tuples_to_strings(follow_k))
-    # print(f)
 
-    # print(first_follow.concat_k(1, {(Terminal('ε'),)}, {(Terminal('ε'),), (Terminal('+'),)}))
+    parse_and_display(test_grammar, str_to_parse)
+
+    print("TEST 2: \n")
+    test_grammar_1 = Grammar(Grammar.read_grammar_from_file("inputs/input_1.txt"))
+    str_to_parse_1 = 'id + !'
+    parse_and_display(test_grammar_1, str_to_parse_1)
 
 
-    test_grammar_2 = Grammar({
-        'E': ['id + D', '( E * R )', 'ε'],
-        'D': ['V * E', 'L ! E'],
-        'R': ['V ! E'],
-        'V': ['Z', 'num'],
-        'L': ['Z', '( E )'],
-        'Z': ['ε']
-    })
 
-    #test_grammar_3 = Grammar(Grammar.read_grammar_from_file("inputs/input_1.txt"))
-    # print(test_grammar.non_terminals)
 
-    print(test_grammar.epsilon_producers)
-    print(test_grammar_2.epsilon_producers)
-    #print(test_grammar_3.epsilon_producers)
-      
 
